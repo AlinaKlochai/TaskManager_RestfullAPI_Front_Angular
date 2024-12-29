34ffd481f305f783692bacd2eb7bb71babfb1666
AlinaKlochai <mrs.ali@tuta.io>
2024-12-29 14:37:58 +0100

    29.12.24

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,54 +1,3 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common'; // Импортируем CommonModule для использования директив (например, ngFor)
-// import { FormsModule } from '@angular/forms'; // Для ngModel
-// import { TodoService } from '../services/todoServise';  // Для работы с сервисами
-// import { Todo } from '../models/todo'; 
-// import { HttpClientModule } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-todo-list',
-//   standalone: true,  // Указываем, что это standalone компонент
-//   imports: [HttpClientModule, CommonModule, FormsModule], // Добавляем FormsModule
-//   templateUrl: './todo-list.component.html',
-//   styleUrls: ['./todo-list.component.css']
-// })
-// export class TodoListComponent {
-//   todos: Todo[] = [];
-//   newTodo: Partial<Todo> = { description: '', dueDate: '' };
-
-//   constructor(private todoService: TodoService) {}
-
-//   ngOnInit(): void {
-//     this.loadTodos();
-//   }
-
-//   loadTodos(): void {
-//     this.todoService.getTodos().subscribe((data) => {
-//       this.todos = data;
-//     });
-//   }
-
-//   addTodo(): void {
-//     this.todoService.createTodo(this.newTodo).subscribe(() => {
-//       this.loadTodos();
-//       this.newTodo = { description: '', dueDate: '' };
-//     });
-//   }
-
-//   deleteTodo(id: number): void {
-//     this.todoService.deleteTodo(id).subscribe(() => {
-//       this.loadTodos();
-//     });
-//   }
-
-//   markAsDone(id: number): void {
-//     this.todoService.updateStatus(id).subscribe({
-//       next: () => this.loadTodos(),
-//       error: (err) => console.error('Error with update status for current todo', err)
-//     });
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Импортируем CommonModule для использования директив (например, ngFor)
 import { FormsModule } from '@angular/forms'; // Для ngModel
@@ -66,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
   newTodo: Partial<Todo> = { description: '', dueDate: '' };
+  errorMessage: string = ''; // Поле для хранения ошибки
 
   constructor(private todoService: TodoService) {}
 
@@ -79,12 +29,34 @@ export class TodoListComponent implements OnInit {
     });
   }
 
-  addTodo(): void {
-    this.todoService.createTodo(this.newTodo).subscribe(() => {
+  // addTodo(): void {
+  //   this.todoService.createTodo(this.newTodo).subscribe(() => {
+  //     this.loadTodos();
+  //     this.newTodo = { description: '', dueDate: '' }; // Сбросить форму
+  //   });
+  // }
+
+addTodo(): void {
+  this.todoService.createTodo(this.newTodo).subscribe({
+    next: () => {
       this.loadTodos();
       this.newTodo = { description: '', dueDate: '' }; // Сбросить форму
-    });
-  }
+      this.errorMessage = ''; // Сбросить ошибку
+    },
+    error: (err) => {
+      if (err.error && err.error.errors) {
+        const descriptionError = err.error.errors.find(
+          (e: any) => e.field === 'description'
+        );
+        if (descriptionError) {
+          this.errorMessage = descriptionError.message;
+        }
+      } else {
+        this.errorMessage = 'An unexpected error occurred.';
+      }
+    },
+  });
+}
 
   deleteTodo(id: number): void {
     this.todoService.deleteTodo(id).subscribe(() => {
